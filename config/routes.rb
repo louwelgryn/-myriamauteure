7b5100d9c175974fe65e697a741a68c48e185418
Lou Welgryn <lou.welgryn@gmail.com>
2019-10-08 16:54:38 +0200

    improved navbar

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     resources :critics, only: [:create, :index]
     resources :reader_comments, only: [:create, :index]
   end
-  root 'home#index'
+  root 'pages#homepage'
   resources :home, only: [:index, :new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
