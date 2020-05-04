09d761174bfc9372cff65d5b5af51369a5d388e2
louwelgryn <lou@getaround.com>
2020-05-04 23:43:58 +0200

    responsive

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   get 'tweets', to: 'pages#tweets', as: :tweets
   get 'bio', to: 'pages#bio', as: :bio
   get 'dessin', to: 'pages#dessin', as: :dessin
+  get 'video', to: 'pages#video', as: :video
   resources :home, only: [:index, :new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
