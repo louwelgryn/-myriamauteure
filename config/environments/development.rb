ce4dcd5d2f36ceca7475652a5965a5802a6c1045
louwelgryn <lou@getaround.com>
2020-05-05 22:54:41 +0200

    final changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -65,9 +65,9 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'gmail.com',
-    user_name:            ENV["GMAIL_EMAIL"],
-    password:             ENV["GMAIL_PASSWORD"],
+    domain:               'gmail',
+    user_name:            'myriamcohenwelgrynauteure@gmail.com',
+    password:             'Mjlydle1!',
     authentication:       "plain",
     enable_starttls_auto: true  }
 end
